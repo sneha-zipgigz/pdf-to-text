b07044fc8aad274bf4369e67f044c751e17f29ca
sneha-zipgigz <81689898+sneha-zipgigz@users.noreply.github.com>
2021-04-15 19:10:35 +0530

    Add files via upload

---
+++ Convert_pdftotext.py
@@ -0,0 +1,25 @@
+import pdfminer
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def pdf_to_text(file_name):
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    pagenums = set()
+    infile = open(file_name, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close()
+
+    
+    return text
